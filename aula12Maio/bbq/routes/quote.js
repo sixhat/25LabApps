10ddaa42cc48f309dffa02470d452469aaddd814
Dave <sixhat@users.noreply.github.com>
2025-05-14 11:30:08 +0100

    Update quote.js

--- aula12Maio/bbq/routes/quote.js
+++ aula12Maio/bbq/routes/quote.js
@@ -2,10 +2,10 @@ var express = require('express');
 var axios = require('axios');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET quote */
 router.get('/', async function(req, res, next) {
-
-  const resultados = await axios.get('https://api.breakingbadquotes.xyz/v1/quotes');
+  
+	const resultados = await axios.get('https://api.breakingbadquotes.xyz/v1/quotes');
 
   res.render('quote', {
       ...resultados.data[0] ,
